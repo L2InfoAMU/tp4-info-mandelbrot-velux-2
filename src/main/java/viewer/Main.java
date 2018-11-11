c72f9a9041dc194bb40f1cb3f9a8bc5bc162de58
Burnasse <tommy.lecourt@etu.univ-amu.fr>
2018-11-11 15:13:01 +0100

    Ajout d'une fenetre avant l'affichage de Mandelbrot     -Ajout de spinner via la class Spinners pour choisir les différents     attributs de la camera.     -Ajout de ColorPicker pour pouvoir changer la couleur des différents     éléments de Mandelbrot.     -Ajout d'un bouton Start pour afficher l'ensemble de Mandelbrot avec     les nouveaux parametres choisis.

--- src/main/java/viewer/Main.java
+++ src/main/java/viewer/Main.java
@@ -2,17 +2,121 @@ package viewer;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/viewer/viewer.fxml"));
-        primaryStage.setTitle("Mandelbrot");
-        primaryStage.setScene(new Scene(root, 1200, 900));
+    public void start(Stage primaryStage){
+        Group preRoot = new Group();
+
+        /*Display ColorPicker*/
+        final ColorPicker colorPicker1 = new ColorPicker(Color.rgb(55, 118, 145));
+        colorPicker1.setTranslateX(300);
+        colorPicker1.setTranslateY(30);
+
+        final ColorPicker colorPicker2 = new ColorPicker(Color.rgb(63, 74, 132));
+        colorPicker2.setTranslateX(300);
+        colorPicker2.setTranslateY(60);
+
+        final ColorPicker colorPicker3 = new ColorPicker(Color.rgb(145, 121, 82));
+        colorPicker3.setTranslateX(300);
+        colorPicker3.setTranslateY(90);
+
+        final ColorPicker colorPicker4 = new ColorPicker(Color.gray(0.7));
+        colorPicker4.setTranslateX(300);
+        colorPicker4.setTranslateY(120);
+
+        preRoot.getChildren().addAll(colorPicker1,colorPicker2,colorPicker3,colorPicker4);
+
+
+        /*Display description text*/
+        DescText descColor = new DescText("Color:","impact",25,Color.GREY,300,25);
+        DescText descCamera = new DescText("Camera:","impact",25,Color.GREY,10,25);
+        DescText descCenterX = new DescText("= centerX","impact",16,Color.GREY,160,50);
+        DescText descCenterY = new DescText("= CenterY","impact",16,Color.GREY,160,80);
+        DescText descWidth = new DescText("= Width","impact",16,Color.GREY,160,110);
+        DescText descRatio = new DescText("= Ratio","impact",16,Color.GREY,160,140);
+
+        preRoot.getChildren().addAll(
+                descColor.getDescText(),
+                descCamera.getDescText(),
+                descCenterX.getDescText(),
+                descCenterY.getDescText(),
+                descWidth.getDescText(),
+                descRatio.getDescText()
+        );
+
+
+        /*Spinner for Camera*/
+        Spinners centerX = new Spinners(10,30,-2.0,5.0,-0.5);
+        Spinners centerY = new Spinners(10,60,-2.0,5.0,0.0);
+        Spinners width = new Spinners(10,90,-2.0,5.0,3.0);
+        Spinners ratio = new Spinners(10,120,1.0,3.0,4./3.0);
+
+        preRoot.getChildren().addAll(
+                centerX.getSpinner(),
+                centerY.getSpinner(),
+                width.getSpinner(),
+                ratio.getSpinner()
+        );
+
+        /*ColorPicker for Color*/
+
+
+
+        /*Display Start Button*/
+        Buttons start = new Buttons("Start",480,50,100,50);
+        preRoot.getChildren().add(start);
+
+        /*Display scene*/
+        primaryStage.setScene(new Scene(preRoot,600,150));
+
+
+        /*Start Controller*/
+        start.setOnMouseClicked((MouseEvent event) -> {
+                    if (event.getButton().equals(MouseButton.PRIMARY)) {
+                        {
+                            Parent root = null;
+                            try {
+                                /*Initialize camera*/
+                                Camera.camera0 = new Camera(
+                                        centerX.getSpinner().getValue(),
+                                        centerY.getSpinner().getValue(),
+                                        width.getSpinner().getValue(),
+                                        ratio.getSpinner().getValue());
+
+                                /*Initialize mandelbrot color*/
+                                ColorList newCcolor = new ColorList(
+                                        colorPicker1.getValue(),
+                                        colorPicker2.getValue(),
+                                        colorPicker3.getValue(),
+                                        colorPicker4.getValue()
+                                );
+                                ColorList.color0 = newCcolor.getColor();
+
+                                root = FXMLLoader.load(getClass().getResource("/viewer/viewer.fxml"));
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            primaryStage.setTitle("Mandelbrot");
+                            primaryStage.setScene(new Scene(root, 1200, 900));
+                        }
+                    }
+                }
+        );
+
+
         primaryStage.show();
     }
 
