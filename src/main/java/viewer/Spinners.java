3da6537188969eabfada22cfa94f5975ed3f39ef
Burnasse <tommy.lecourt@etu.univ-amu.fr>
2018-11-12 11:30:54 +0100

    Ajout de la method setDefault a la class Spinners afin de remettre sa valeurs d'origine.

--- src/main/java/viewer/Spinners.java
+++ src/main/java/viewer/Spinners.java
@@ -8,12 +8,18 @@ public class Spinners {
 
     final Spinner<Double> spinner = new Spinner<Double>();
     private double initialValue;
+    private double min;
+    private double max;
     private SpinnerValueFactory<Double> valueFactory;
 
-    public Spinners(int transX, int transY, Double min,Double max,double initialValue){
+    public Spinners(int transX, int transY, double min,double max,double initialValue){
         spinner.setTranslateX(transX);
         spinner.setTranslateY(transY);
+
+        this.min = min;
+        this.max = max;
         this.initialValue = initialValue;
+
         valueFactory =
                 new SpinnerValueFactory.DoubleSpinnerValueFactory(min, max, initialValue);
         ((SpinnerValueFactory.DoubleSpinnerValueFactory) valueFactory).setAmountToStepBy(0.1);
@@ -21,4 +27,11 @@ public class Spinners {
     }
 
     public Spinner<Double> getSpinner(){ return spinner;}
+
+    public void setDefault(){
+        valueFactory =
+                new SpinnerValueFactory.DoubleSpinnerValueFactory(min, max, initialValue);
+        ((SpinnerValueFactory.DoubleSpinnerValueFactory) valueFactory).setAmountToStepBy(0.1);
+        spinner.setValueFactory(valueFactory);
+    }
 }
